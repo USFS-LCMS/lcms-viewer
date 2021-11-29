a504b75c830c9af9626b1f72fe99c72660406aff
Your Name <you@example.com>
2021-11-29 15:39:36 -0700

    RDW template edits

--- scratch/rdw-template.js
+++ scratch/rdw-template.js
@@ -8,16 +8,36 @@ var rdwTemplate = {
 
 	'products':`<p>LCMS produces annual maps depicting change (vegetation loss and vegetation gain), land cover, and land use from 1985 to present that can be used to assist with a wide range of land management applications. With the help of Regional and National Forest staffs we have identified many applications of LCMS data, including forest planning and revision, updating existing vegetation maps, assessing landscape conditions, supporting post-fire recovery, and meeting some broad-scale monitoring requirements.</p>`,
 
-	'data-availability':`<p>The full suite of LCMS products are now available for the Conterminous United States and Southeastern Alaska and will be updated annually.</p>`,
+	'data-availability':`<p>The full suite of LCMS products are now available for the Conterminous United States, Southeastern Alaska, and Puerto Rico-U.S. Virgin Islands. These will be updated annually.</p>`,
 
 	'data-access':`<p>
 						LCMS data are available for viewing and download through the <strong>LCMS Data Explorer</strong> (<a href="https://apps.fs.usda.gov/lcms-viewer/" target="_blank" style="font-weight:normal;">https://apps.fs.usda.gov/lcms-viewer/</a>) and also through the links in the table below.<br><br>The LCMS Data Explorer is a web-based application that provides users the ability to view, analyze, summarize and download LCMS data. A user-friendly set of tools allows users to upload an area of interest and perform pixel- and area-based summaries with a charting feature displaying the results of all LCMS outputs. A tutorial is available in the <em>Viewer Support</em> menu.<br><br>For questions or more information on LCMS please contact the LCMS helpdesk <a href="mailto:sm.fs.lcms@usda.gov" style="font-weight:normal;">sm.fs.lcms@usda.gov</a>.
 					</p>`,
 
 	'use-constraints':`<p>
-						These data were collected using funding from the U.S. Government and can be used without additional permissions or fees. If you use these data in a publication, presentation, or other research product please use the following citation:
+						These data were collected using funding from the U.S. Government and can be used without additional permissions or fees. If you use these data in a publication, presentation, or other research product please use the appropriate citation from this table:
 						</p>
-        				<p>USDA Forest Service. 2021. USFS Landscape Change Monitoring System Southeastern Alaska version 2020-5. Salt Lake City, Utah.</p>
+        				<table>
+        					<tr>
+        						<th>Study Area Name</th>
+        						<th>Citation</th>
+        					</tr>
+        					<tr>
+        						<td>Conterminous United States</td>
+        						<td>USDA Forest Service. 2021. USFS Landscape Change Monitoring System Conterminous United States version 2020-5. Salt Lake City, Utah.
+        						</td>
+        					</tr>
+        					<tr>
+        						<td>Southeastern Alaska</td>
+        						<td>USDA Forest Service. 2021. USFS Landscape Change Monitoring System Southeastern Alaska version 2020-5. Salt Lake City, Utah.
+        						</td>
+        					</tr>
+        					<tr>
+        						<td>Puerto Rico-US Virgin Islands</td>
+        						<td>USDA Forest Service. 2021. USFS Landscape Change Monitoring System Puerto Rico-US Virgin Islands version 2020-6. Salt Lake City, Utah.
+        						</td>
+        					</tr>
+        				</table>
         				<p>Appropriate use includes regional to national assessments of vegetation cover, land cover, or land use change trends, total extent of vegetation cover, land cover, or land use change, and aggregated summaries of vegetation cover, land cover, or land use change. This product is the initial output from the modeling process. No post-processing (such as applying a minimum mapping unit or manually burning in known features such as roads) has been performed.</p>`  
 
 }
