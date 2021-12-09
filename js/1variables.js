995100fbe0879a0b03734063b434aa97b2c43944
Your Name <you@example.com>
2021-12-09 15:09:53 -0700

    Updated v6

--- js/1variables.js
+++ js/1variables.js
@@ -425,7 +425,7 @@ var studyAreaDict = {
                                 5: {'modelName': 'Other','legendName': 'Other','color': 'a1a1a1'},
                                 6: {'modelName': 'Rangeland','legendName': 'Rangeland or Pasture','color': 'c2b34a'}},
                  
-                      final_collections  : ['USFS/GTAC/LCMS/v2020-5','projects/lcms-292214/assets/Final_Outputs/2020-6/LCMS'],
+                      final_collections  : ['USFS/GTAC/LCMS/v2020-5','USFS/GTAC/LCMS/v2020-6'],
                       composite_collections : ['projects/USFS/LCMS-NFS/R10/CoastalAK/Composites/Composite-Collection', 'projects/lcms-tcc-shared/assets/Composites/Composite-Collection-yesL7-1984-2020','projects/lcms-292214/assets/R8/PR_USVI/Composites/Composite-Collection-1984-2020'],
                       lt_collections: ['projects/lcms-292214/assets/R10/CoastalAK/Base-Learners/LANDTRENDR-Collection-1984-2020','projects/lcms-tcc-shared/assets/LandTrendr/LandTrendr-Collection-yesL7-1984-2020','projects/lcms-292214/assets/R8/PR_USVI/Base-Learners/LandTrendr-Collection-1984-2020'],
                       ccdc_collections:['projects/lcms-292214/assets/R10/CoastalAK/Base-Learners/CCDC-Collection-1984-2021','projects/lcms-292214/assets/CONUS-LCMS/Base-Learners/CCDC-Collection-1984-2021','projects/lcms-292214/assets/R8/PR_USVI/Base-Learners/CCDC-Landsat-1984-2020']
