2641b61bf853f716a6228f675e40cfb403702f24
Hardwick <12001005040677@FEDIDCARD.GOV>
2023-09-07 13:42:17 -0600

    updates for WDEQ model for Algal mapper

--- js/8gee-run-manager.js
+++ js/8gee-run-manager.js
@@ -3291,23 +3291,31 @@ function runAlgal(){
   
   let ab = ee.ImageCollection('projects/gtac-algal-blooms/assets/outputs/HAB-RF-Images');
   //ab = ab.filter(ee.Filter.eq('studyAreaName',"WY-MT-CO-UT-ID2"))
-  ab = ab.filter(ee.Filter.eq('studyAreaName','GYE-WY'))
+  ab = ab.filter(ee.Filter.eq('studyAreaName','WY-GYE'))
   // console.log(ab.first().getInfo())
  
   ab = ab.filter(ee.Filter.calendarRange(parseInt(urlParams.startYear),parseInt(urlParams.endYear),'year'))
-      .filter(ee.Filter.calendarRange(150,350))
+      .filter(ee.Filter.calendarRange(150,300))
+
+  // Add filter for asset whichModel property (WDEQ vs HCB)
+  ab_wdeq = ab.filter(ee.Filter.eq('whichModel','WDEQ'));
+  ab_hcb = ab.filter(ee.Filter.eq('whichModel','HCB'));
+
   let algalLegendDict={'Algal Negative':'00D','Algal Positive':'D00'};
   // Map2.addTimeLapse(ab.select([0]),{'min':1,'max':2,'palette':'00D,D00','classLegendDict':algalLegendDict,'dateFormat':'YYMMdd','advanceInterval':'day'},'Algal Bloom Classification',true)
 
-   countC = ab.select([0])
+   // countC = ab.select([0]) 
+   countC_hcb = ab_hcb.select([0]); 
+   countC_wdeq = ab_wdeq.select([0]);
   //  countNotC = countC.map(img=>img.updateMask(img.lt(25000)))
   //  countC = countC.map(img=>img.updateMask(img.gte(25000)))
 
-    Map2.addTimeLapse(countC,{'min':25000,'max':5000000,'palette':palettes.matplotlib.plasma[7],'dateFormat':'YYMMdd','advanceInterval':'day','dateField':'system:time_end',legendNumbersWithCommas:true},'Cyanobacteria Count',true,'cells/mL');
+    Map2.addTimeLapse(countC_hcb,{'min':25000,'max':5000000,'palette':palettes.matplotlib.plasma[7],'dateFormat':'YYMMdd','advanceInterval':'day','dateField':'system:time_end',legendNumbersWithCommas:true},'Cyanobacteria Count',true,'cells/mL');
+    Map2.addTimeLapse(ab_hcb.select([1]),{'min':200000000,'max':1000000000,'palette':palettes.matplotlib.plasma[7],'dateFormat':'YYMMdd','advanceInterval':'day','dateField':'system:time_end',legendNumbersWithCommas:true},'Cyanobacteria Biovolume',true,'µm3');
+    
+    Map2.addTimeLapse(countC_wdeq,{'min':1000,'max':5000,'palette':palettes.matplotlib.plasma[7],'dateFormat':'YYMMdd','advanceInterval':'day','dateField':'system:time_end',legendNumbersWithCommas:true},'Cyanophyceae Cell Count',true,'raw cell count');
+    Map2.addTimeLapse(ab_wdeq.select([1]),{'min':2000000,'max':10000000,'palette':palettes.matplotlib.plasma[7],'dateFormat':'YYMMdd','advanceInterval':'day','dateField':'system:time_end',legendNumbersWithCommas:true},'Cyanophyceae Density',true,'cells/L');
 
-    
-    Map2.addTimeLapse(ab.select([1]),{'min':200000000,'max':1000000000,'palette':palettes.matplotlib.plasma[7],'dateFormat':'YYMMdd','advanceInterval':'day','dateField':'system:time_end',legendNumbersWithCommas:true},'Cyanobacteria Biovolume',true,'µm3');
-    
     setTimeout(()=>{$('#Cyanobacteria-Count-1-name-span').click();
       setTimeout(()=>{$('#Cyanobacteria-Count-1-forward-button>i').click();
       $('#Cyanobacteria-Count-1-forward-button>i').click();
